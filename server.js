898c7141dd277b479ba3fb7eed08cdf12753e64e
Mario Pece <mariopece@icloud.com>
2021-01-09 21:44:04 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 

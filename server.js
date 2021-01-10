71e26a0b5d8bc7f4473eb84f18fd18677f2665ea
Mario Pece <mariopece@icloud.com>
2021-01-10 10:38:58 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
   
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/*', (req, res) => {
@@ -9,24 +8,6 @@ app.get('/*', (req, res) => {
 });
 
 
-
-io.on('connection', function(socket){
-
-  var room = socket.handshake['query']['r_var'];
-
-  socket.join(room);
-  console.log('user joined room #'+room);
-
-  socket.on('disconnect', function() {
-    socket.leave(room)
-    console.log('user disconnected');
-  });
-
-  socket.on('chat message', function(msg){
-    io.to(room).emit('chat message', msg);
-  });
-
-});
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);

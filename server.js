88295b32ce00f28c6bc967d436f24cb19a1f9e3a
Mario Pece <mariopece@icloud.com>
2021-01-09 18:46:33 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,8 @@ var app = express()
 app.use(express.static('public'))
 
 var io = require('socket.io').listen(server);
-var server = http.createServer(app);
 
 port = process.env.PORT
 app.listen(port)
 
-
-
-
-
+var server = http.createServer(app);

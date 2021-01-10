ee63c6d792428f165385182a33b6a0abc6097b8f
Mario Pece <mariopece@icloud.com>
2021-01-09 19:42:17 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
-var express = require('express')
-var app = express()
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+  
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 
-http.listen(process.env.PORT || 3000, function() {
-  var host = http.address().address
-  var port = http.address().port
-  console.log('App listening at http://%s:%s', host, port)
-});
-
-io.on('connection', function(socket) {
-  console.log('Client connected to the WebSocket');
-
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
-
-  socket.on('chat message', function(msg) {
-    console.log("Received a chat message");
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
     io.emit('chat message', msg);
   });
-})
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+
+
+

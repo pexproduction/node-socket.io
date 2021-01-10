886b361192ecf2cb2252bf58514cd8d0e2ef58ba
Mario Pece <mariopece@icloud.com>
2021-01-09 18:25:18 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 var express = require('express')
-var fs = require('fs')
 var app = express()
-var bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.get('/index.html', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
-})
+app.use(express.static('public'))
 
 port = process.env.PORT
 app.listen(port)
-console.log(`Listening at http://localhost:${port}`)
